8d22a617b532ec6fed0933587d1fc31d4b6d2920
Meg Richards <merichar@merichar.org>
2026-02-23 22:53:04 -0500

    chore: update .dir-locals.el with fix for flycheck yamllint working-directory
    Set yaml-yamllint's working directory to the nearest ancestor containing
    .yamllint so ignore-from-file: .gitignore resolves correctly even in deeply
    nested subdirectories.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,7 @@
 ((yaml-mode . ((flycheck-checker . yaml-yamllint)
-               (lsp-yaml-custom-tags . ["!secret" "!include" "!lambda" "!extend"]))))
+               (lsp-yaml-custom-tags . ["!secret" "!include" "!lambda" "!extend"])
+               (eval . (with-eval-after-load 'flycheck
+                         (setf (flycheck-checker-get 'yaml-yamllint 'working-directory)
+                               (lambda (_checker)
+                                 (and buffer-file-name
+                                      (locate-dominating-file buffer-file-name ".yamllint")))))))))
